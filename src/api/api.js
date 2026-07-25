ec7beb7eef3a12d6b54431aace999a1ef2ecf70e
khan47650 <bsem-f20-110@superior.edu.pk>
2026-07-25 12:07:21 +0500

    some changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://khan-moves-backend.vercel.app/api",
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://khan-moves-backend.vercel.app/api",
+    // baseURL: "http://localhost:5000/api",
 });
 
 const TOKEN_KEY = "khanmoves_token";
